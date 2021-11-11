2b2240ce3f8426ee5449554d02b13fb0bd495029
Yasroslav <slavles04@gmail.com>
2021-11-11 13:46:53 +0200

    Added Entities to DAL

---
+++ DAL/Entities/Disease.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace SDTech\DAL\Entities;
+
+
+class Disease
+{
+    public int $id;
+    public string $name;
+    public int $caseAmount;
+}
---
+++ DAL/Entities/Locality.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace SDTech\DAL\Entities;
+
+
+class Locality
+{
+    public int $id;
+    public string $name;
+    public int $population;
+    public array $diseases;
+}
